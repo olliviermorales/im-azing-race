824f2d5b29319fcaffae70efa98cff94d75bab56
Ollivier Julles Morales <morales.ollivier@gmail.com>
2023-10-15 13:15:27 +0800

    Push

--- app/api/users/getAll/route.js
+++ app/api/users/getAll/route.js
@@ -1,23 +1,18 @@
 import { connectToDB } from '@/utils/database';
 import Users from '@/models/user';
 
-export default async (req, res) => {
-  if (req.method === 'GET') {
-    try {
-      await connectToDB();
+export const GET = async (req) => {
+  try {
+    await connectToDB();
 
-      const users = await Users.find();
+    const users = await Users.find();
 
-      res.setHeader(
-        'Cache-Control',
-        'no-store, no-cache, must-revalidate, proxy-revalidate'
-      );
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({ message: 'Failed to fetch all users' });
-    }
-  } else {
-    // Handle other HTTP methods
-    res.status(405).json({ message: 'Method Not Allowed' });
+    res.setHeader(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, proxy-revalidate'
+    );
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to fetch all users' });
   }
 };
