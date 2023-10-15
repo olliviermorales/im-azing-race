476c194369c8e4de5b50f45adebc07e16e1cc6ec
Ollivier Julles Morales <morales.ollivier@gmail.com>
2023-10-15 13:19:33 +0800

    push

--- app/api/users/getAll/route.js
+++ app/api/users/getAll/route.js
@@ -1,18 +1,31 @@
-import { connectToDB } from '@/utils/database';
-import Users from '@/models/user';
-
-export const GET = async (req, res) => {
+export const GET = async (req) => {
   try {
     await connectToDB();
 
+    // Fetch the timestamp of the last modified user
+    const lastModifiedUser = await Users.findOne()
+      .sort({ updatedAt: -1 })
+      .select('updatedAt');
+    const lastModifiedTimestamp = lastModifiedUser?.updatedAt;
+
+    // If there's an If-Modified-Since header, compare the dates
+    const clientTimestamp = new Date(req.headers.get('If-Modified-Since'));
+    if (lastModifiedTimestamp && clientTimestamp >= lastModifiedTimestamp) {
+      return new Response(null, { status: 304 }); // No need to send data, just the status
+    }
+
     const users = await Users.find();
 
-    res.setHeader(
-      'Cache-Control',
-      'no-store, no-cache, must-revalidate, proxy-revalidate'
-    );
-    res.status(200).json(users);
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+
+    if (lastModifiedTimestamp) {
+      headers['Last-Modified'] = lastModifiedTimestamp.toUTCString();
+    }
+
+    return new Response(JSON.stringify(users), { status: 200, headers });
   } catch (error) {
-    res.status(500).json({ message: 'Failed to fetch all users' });
+    return new Response('Failed to fetch all users', { status: 500 });
   }
 };
