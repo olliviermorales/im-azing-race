31218cc90a25ce9566efe133ddb7f590414798f1
Ollivier Julles Morales <morales.ollivier@gmail.com>
2023-10-15 13:12:30 +0800

    Updated get method

--- app/api/users/getAll/route.js
+++ app/api/users/getAll/route.js
@@ -1,14 +1,23 @@
 import { connectToDB } from '@/utils/database';
 import Users from '@/models/user';
 
-export const GET = async (req) => {
-  try {
-    await connectToDB();
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      await connectToDB();
 
-    const users = await Users.find();
+      const users = await Users.find();
 
-    return new Response(JSON.stringify(users), { status: 200 });
-  } catch (error) {
-    return new Response('Failed to fetch all users', { status: 500 });
+      res.setHeader(
+        'Cache-Control',
+        'no-store, no-cache, must-revalidate, proxy-revalidate'
+      );
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to fetch all users' });
+    }
+  } else {
+    // Handle other HTTP methods
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 };
